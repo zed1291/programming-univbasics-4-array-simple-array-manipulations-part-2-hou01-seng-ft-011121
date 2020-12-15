5dcd38ef59b4f4c8c211fd921f8420562d26d86f
zed <zed@kolk.me>
2020-12-15 21:46:26 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,11 @@
+def using_concat array1, array2
+  array1 = array1.concat(array2)
+end
+
+def using_insert array, element
+  array = array.insert(4, element)
+end
+
+def using_uniq array
+  array = array.flatten
+end
